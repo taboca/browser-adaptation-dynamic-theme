00ec6bfb71818b7bf3bc2dd9b3ff95cac61967c4
Marcio Galli <mgalli@taboca.com>
2018-04-14 17:58:53 -0300

    Content script for meta-data

---
+++ src/body.js
@@ -0,0 +1,25 @@
+//document.body.style.border = "5px solid red";
+
+//browser.runtime.sendMessage(document.body.backgroundColor);
+/*
+
+console.log('From the extension body.js');
+
+function _metaChangedHandler(e) {
+  console.log('Meta added');
+  document.body.style.border = "5px solid blue";
+}
+
+addEventListener('DOMMetaAdded', _metaChangedHandler, true);
+
+console.log('meta:' + document.getElementById('theme-color').getAttribute('content'));
+
+*/
+
+
+var metaData = {
+  kind  : 'theme-color',
+  value :  document.getElementById('theme-color').getAttribute('content')
+}
+
+browser.runtime.sendMessage(metaData);
